5daca933eafed7506f0cf9a754feadc5ceb1fac2
danielrubens <danielrubensdaniel@gmail.com>
2023-02-11 18:19:10 -0300

    useMemo to create theme

--- src/theme.js
+++ src/theme.js
@@ -190,3 +190,21 @@ export const tokens = (mode) => ({
         }
     }
   }
+
+// context for color mode
+
+export const colorModelContext = createContext({
+    toggleColorMode: () => {
+
+    }
+})
+
+export const useMode = () => {
+    const [mode, setMode] = useState('dark')
+    const colorMode = useMemo(() => ({
+        toggleColorMode: () => setMode((prev) => (prev === 'light' ? 'dark': 'light'))
+    }), [])
+
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+    return [theme, colorMode]
+}

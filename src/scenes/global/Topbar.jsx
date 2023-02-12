4776a7108c7049f04acc12540220b90fdd34f4cd
danielrubens <danielrubensdaniel@gmail.com>
2023-02-12 13:50:04 -0300

    dark and light mode onclick implemented

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -18,8 +18,22 @@ const Topbar = () => {
    <Box display="flex" justifyContent="space-between" p={2}>
     <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
       <InputBase sx={{ml: 2, flex:1}} placeholder="Search"/>
+      <IconButton type="button" sx={{p:1}}>
+        <SearchIcon />
+      </IconButton>
     </Box>
-    <IconButton></IconButton>
+    <Box display="flex">
+      <IconButton onClick={colorMode.toggleColorMode}>
+        {theme.palette.mode === 'dark' ?  <DarkModeOutlinedIcon/> : <LightModeOutlinedIcon />}
+      </IconButton>
+      <IconButton><NotificationsOutlinedIcon /></IconButton>
+      <IconButton><SettingsOutlinedIcon /></IconButton>
+      <IconButton><PersonOutlinedIcon/></IconButton>
+      <IconButton>
+
+        
+      </IconButton>
+    </Box>
    </Box>
   )
 }

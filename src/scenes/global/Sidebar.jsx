5c4807a31b86be6803f6288478817ea3ea44a460
danielrubens <danielrubensdaniel@gmail.com>
2023-02-12 19:01:46 -0300

    init prosidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -24,10 +24,53 @@ const Sidebar = () => {
   const [selected, setSelected] = useState('Dashboard')
 
   return (
-    <Box sx={{"& .pro-sidebar-inner": {
-      background: `${colors.primary[400]} !important`
-    } }}>
-
+    <Box 
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`
+        },
+        "& .pro-icon-wrapper": {
+          background: "transparent !important"
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important"
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important"
+        } ,
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important"
+        }
+        }}
+        >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+        </Menu>
+      </ProSidebar>
     </Box>
   )
 }

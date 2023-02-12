239b77b8467674686f5c5b1557639e72932720f0
danielrubens <danielrubensdaniel@gmail.com>
2023-02-12 18:30:09 -0300

    sidebar import elements

--- src/App.js
+++ src/App.js
@@ -24,11 +24,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
           <div className="app">
+            <Sidebar />
             <main className="content">
              <Topbar />
              <Routes>
               <Route exact path="/" element={<Dashboard />}/>
-              <Route path="/team" element={<Team />}/>
+              {/* <Route path="/team" element={<Team />}/>
               <Route path="/contacts" element={<Contacts />}/>
               <Route path="/invoices" element={<Invoices />}/>
               <Route path="/form" element={<Form />}/>
@@ -37,8 +38,7 @@ function App() {
               <Route path="/line" element={<Line />}/>
               <Route path="/faq" element={<FAQ />}/>
               <Route path="/geography" element={<Geography />}/>
-              <Route path="/calendar" element={<Calendar />}/>
-
+              <Route path="/calendar" element={<Calendar />}/> */}
              </Routes>
             </main>
           </div>

2dfdb6d4dc437d969f4d91fa216bbd00a9ae58b2
danielrubens <danielrubensdaniel@gmail.com>
2023-02-12 13:41:03 -0300

    search bar on InputBase

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,7 +1,7 @@
 import {Box, IconButton, useTheme} from '@mui/material'
 import {useContext} from 'react'
 import { ColorModeContext, tokens } from '../../theme'
-import InputBase from '@mui/material'
+import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
@@ -16,6 +16,9 @@ const Topbar = () => {
   const colorMode = useContext(ColorModeContext)
   return (
    <Box display="flex" justifyContent="space-between" p={2}>
+    <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+      <InputBase sx={{ml: 2, flex:1}} placeholder="Search"/>
+    </Box>
     <IconButton></IconButton>
    </Box>
   )

b7105c31850c34c1654aacd503880345627bf4ff
danielrubens <danielrubensdaniel@gmail.com>
2023-02-12 22:53:06 -0300

    data grid table on teams

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
-
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -30,8 +29,8 @@ function App() {
              <Topbar />
              <Routes>
               <Route exact path="/" element={<Dashboard />}/>
-              {/* <Route path="/team" element={<Team />}/>
-              <Route path="/contacts" element={<Contacts />}/>
+              {<Route path="/team" element={<Team />}/>}
+              {/* <Route path="/contacts" element={<Contacts />}/>
               <Route path="/invoices" element={<Invoices />}/>
               <Route path="/form" element={<Form />}/>
               <Route path="/bar" element={<Bar />}/>
@@ -39,7 +38,7 @@ function App() {
               <Route path="/line" element={<Line />}/>
               <Route path="/faq" element={<FAQ />}/>
               <Route path="/geography" element={<Geography />}/>
-              <Route path="/calendar" element={<Calendar />}/> */}
+              <Route path="/calendar" element={<Calendar />}/> */} */}
              </Routes>
             </main>
           </div>

b4dd247fe252613cf404f9a301534e236d537fa8
danielrubens <danielrubensdaniel@gmail.com>
2023-02-13 20:22:22 -0300

    calendar component started

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -4,7 +4,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
-import { Box, List, ListITem, ListItemText, Typography, useTheme } from '@mui/material';
+import { Box, List, ListItem, ListItemText, Typography, useTheme } from '@mui/material';
 import Header from '../../components/Header';
 import { tokens } from '../../theme';
 
@@ -34,7 +34,30 @@ const Calendar = () => {
     }
   }
   return (
-    <div>Calendar</div>
+    <Box m="20px">
+        <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+        <Box display="flex" justifyContent="space-between">
+            {/* {Calendar Sidebar} */}
+            <Box flex="1 1 20%" backgroundColor={colors.primary[400]} p="15px" borderRadius="4px">
+                <Typography variant="h5">Events</Typography>
+                <List>
+                    {currentEvents.map((i) => (
+                        <ListItem key={i.id} 
+                        sx={{backgroundColor: colors.greenAccent[500], margin: "10px 0", borderRadius: "2px"}}>
+                            <ListItemText primary={i.title} secondary={
+                                <Typography>
+                                    {formatDate(i.start, {
+                                        year: "numeric", month: "short", day: "numeric"
+                                    })}
+                                </Typography>
+                            }
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+        </Box>
+    </Box>
   )
 }
 

661cbac3638bc4ee4b31a24a60b7e8ac5718d32c
danielrubens <danielrubensdaniel@gmail.com>
2023-02-14 15:39:01 -0300

    lineChart component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contacts from "./scenes/contacts";
 import Bar from "./components/BarChart";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
 import FAQ from './scenes/faq';
 // import Geography from '../scenes/geography';
 import Calendar from './scenes/calendar';
@@ -36,8 +36,8 @@ function App() {
               <Route path="/calendar" element={<Calendar />}/>
               <Route path="/faq" element={<FAQ />}/>
               <Route path="/bar" element={<Bar />}/>
-              {/* <Route path="/pie" element={<Pie />}/>
-              <Route path="/line" element={<Line />}/>
+              <Route path="/pie" element={<Pie />}/>
+              {/* <Route path="/line" element={<Line />}/>
               <Route path="/geography" element={<Geography />}/> */}
              </Routes>
             </main>

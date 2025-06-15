d14793c2ae7df027422e071d0b779a272949247a
daniel <danielrubensdaniel@gmail.com>
2025-06-15 18:52:52 -0300

    Replace Sales Quantity with Denial Reason

--- src/scenes/Dashboard.jsx
+++ src/scenes/Dashboard.jsx
@@ -247,7 +247,7 @@ const Dashboard = () => {
             fontWeight="600"
             sx={{ padding: "30px 30px 0 30px" }}
           >
-            Sales Quantity
+            Denial Reason
           </Typography>
           <Box height="250px" mt="-20px">
             <BarChart isDashboard={true} />
